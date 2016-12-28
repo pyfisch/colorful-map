5d7f6267779117a6c44ba2d30e4e789ca341485e
Pyfisch <pyfisch@gmail.com>
2016-12-28 20:55:53 +0100

    Fix memory leak. Rewrite glue.js

--- examples/wrapper.rs
+++ examples/wrapper.rs
@@ -8,7 +8,7 @@ use std::slice;
 
 use colorful_map::process;
 
-#[link_args = "-s EXPORTED_FUNCTIONS=['_process_web']"]
+#[link_args = "-s EXPORTED_FUNCTIONS=['_process_web','_free_cstring_web']"]
 extern {}
 
 #[no_mangle]
@@ -16,11 +16,16 @@ pub extern fn process_web(p: *const u8, len: usize) -> *const c_char {
     let input = unsafe {
         slice::from_raw_parts(p, len)
     };
-    let output = process(input).unwrap();
-    let c_string = CString::new(output).unwrap();
-    let ptr = c_string.as_ptr();
-    std::mem::forget(c_string);
-    ptr
+    let output = process(input).expect("input file is valid");
+    CString::new(output).expect("string contains no internal NULL").into_raw()
+}
+
+#[no_mangle]
+pub extern fn free_cstring_web(p: *mut c_char) {
+    unsafe {
+        if p.is_null() { return }
+        CString::from_raw(p)
+    };
 }
 
 fn main() {
--- web/glue.js
+++ web/glue.js
@@ -1,25 +1,50 @@
-const base = 'https://tile.mapzen.com/mapzen/vector/v1';
-const process_web = Module.cwrap('process_web', 'string', ['number', 'number']);
+// `base` and `api_key` describe the API endpoint.
+const base = 'https://tile.mapzen.com/mapzen/vector/v1/';
+const api_key = 'mapzen-j16kH4C'
 
-var mymap = L.map('mapid').setView([50, 8], 11);
+// Wrapper for Rust functions. Do not use directly.
+const _process = Module.cwrap('process_web', 'number', ['number', 'number']);
+const _free_cstring = Module.cwrap('free_cstring_web', null, ['number']);
 
-var SvgLayer = L.GridLayer.extend({
+// Parses an `mvt` (Mapbox Vector Tile) file and renders its contents
+// as an SVG fragment. The fragment is returned as a string.
+//
+// Wraps the `process` function written in Rust.
+function process(mvt) {
+  const array = new Uint8Array(mvt);
+  const length = array.length;
+  const array_p = Module._malloc(length);
+  Module.writeArrayToMemory(array, array_p);
+  const string_p = _process(array_p, length);
+  const string = UTF8ToString(string_p);
+  _free_cstring(string_p);
+  return string;
+}
+
+// Gets the URL from where to load the vector tile.
+// By default it loads all layers, but you can also give it a layer
+// name or a list of layers to load.
+function getURL(coords, layers='all') {
+  if (Array.isArray(layers)) {
+    layers = layers.join();
+  }
+  return `${ base }${ layers }/${ coords.z }/${ coords.x }/${ coords.y }.mvt?api_key=${ api_key }`;
+}
+
+// The vector tile layer displays vector tiles in the Leaflet window.
+const VectorTileLayer = L.GridLayer.extend({
     createTile: function(coords){
         let tile = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
         const size = this.getTileSize();
         tile.setAttribute('width', size.x);
         tile.setAttribute('height', size.y);
-        fetch(`${ base }/${ ['all'].join() }/${ coords.z }/${ coords.x }/${ coords.y }.mvt?api_key=mapzen-j16kH4C`)
-          .then(function (res) {
-            return res.arrayBuffer();
-          }).then(function (buffer) {
-            let data = new Uint8Array(buffer);
-            let p = Module._malloc(data.length);
-            Module.writeArrayToMemory(data, p);
-            tile.innerHTML = process_web(p, data.length);
-          });
+        fetch(getURL(coords))
+          .then(res => res.arrayBuffer())
+          .then(buffer => tile.innerHTML = process(buffer));
         return tile;
     }
 });
 
-new SvgLayer().addTo(mymap);
+// Creates the map using the div with the id `map`
+let map = L.map('main-map').setView([50, 8], 11);
+new VectorTileLayer().addTo(map);

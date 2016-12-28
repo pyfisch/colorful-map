2e77636f9a7510537c5b7008d15cf8a44e56eafe
Pyfisch <pyfisch@gmail.com>
2016-12-28 23:01:04 +0100

    Change project from lib to bin

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+#![feature(link_args)]
+
 extern crate protobuf;
 
+use std::ffi::CString;
 use std::io::Read;
+use std::os::raw::c_char;
+use std::slice;
 
 use protobuf::ProtobufResult;
 
@@ -53,3 +58,26 @@ pub fn process<R: Read>(mut r: R) -> ProtobufResult<String> {
     }
     Ok(String::from(storage))
 }
+
+#[cfg_attr(target_arch="asmjs",
+           link_args="-s EXPORTED_FUNCTIONS=['_process_web','_free_cstring_web']")]
+extern {}
+
+#[no_mangle]
+pub extern fn process_web(p: *const u8, len: usize) -> *const c_char {
+    let input = unsafe {
+        slice::from_raw_parts(p, len)
+    };
+    let output = process(input).expect("input file is valid");
+    CString::new(output).expect("string contains no internal NULL").into_raw()
+}
+
+#[no_mangle]
+pub extern fn free_cstring_web(p: *mut c_char) {
+    unsafe {
+        if p.is_null() { return }
+        CString::from_raw(p)
+    };
+}
+
+fn main() {}

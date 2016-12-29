5123c28188b904cafa7d216403483fff181df615
Pyfisch <pyfisch@gmail.com>
2016-12-29 13:22:28 +0100

    Improve glue.js (fragment)

--- web/glue.js
+++ web/glue.js
@@ -1,10 +1,12 @@
+// A web worker is used to render the tiles.
 let worker = new Worker('worker.js');
+// The returned strings are added as innerHTML to the given element.
 worker.addEventListener('message',
   function(e) {
     let elem = document.getElementById(e.data.id);
     // Sometimes after rendering the tile is no longer needed.
     if (elem !== null) {
-      elem.innerHTML = e.data.tile
+      elem.innerHTML = e.data.tile;
     }
 });
 
@@ -12,7 +14,7 @@ worker.addEventListener('message',
 //
 // Tiles must be 256x256 pixels in size.
 const VectorTileLayer = L.GridLayer.extend({
-    createTile: function(coords){
+    createTile: function(coords) {
         let tile = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
         tile.setAttribute('width', 256);
         tile.setAttribute('height', 256);
@@ -22,6 +24,42 @@ const VectorTileLayer = L.GridLayer.extend({
     }
 });
 
+/// Parses the URL fragment and returns position and zoom.
+function parseFragment(standard=null) {
+  const expr = /map=(\d+)\/([\d\.]+)\/([\d\.]+)/;
+  const match = location.hash.match(expr);
+  if (match !== null) {
+    return [[Number(match[2]), Number(match[3])], Number(match[1])];
+  } else {
+    return standard;
+  }
+}
+
 // Creates the map using the div with the id `map`
-let map = L.map('main-map').setView([50, 8], 11);
+// The map is set to the position from the fragment
+// or is centered on Frankfurt (Main), Germany.
+let map = L.map('main-map');
+const view = parseFragment([[50.1, 8.6], 12])
+map.setView(view[0], view[1]);
 new VectorTileLayer().addTo(map);
+// After the map finishes a move (or zoom)
+// the fragment is updated with the new location.
+map.on('moveend', function(e) {
+  const center = map.getCenter();
+  const lat = center.lat.toFixed(4);
+  const lng = center.lng.toFixed(4);
+  const zoom = map.getZoom();
+  location.hash = `map=${ zoom }/${ lat }/${ lng }`
+})
+// If the user changes the fragment the map is updated.
+window.onhashchange = function(e) {
+  const view = parseFragment();
+  if (view !== null) {
+    const center = map.getCenter();
+    const delta = Math.abs(center.lat - view[0][0])
+        + Math.abs(center.lng - view[0][1]);
+    if (delta > 0.001 || map.getZoom() !== view[1]) {
+      map.setView(view[0], view[1]);
+    }
+  }
+}
